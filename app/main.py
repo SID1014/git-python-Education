fe6d649820ceacdf5a48775edb8dcf91e913cd3c
SID1014 <siddhantdakhore143@gmail.com>
2026-03-03 19:09:19 +0530

    Fifth Stage

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import hashlib
 
 #write tree function for recursion in directory and create data to write in file
 def write_tree():
-    print(Path.iterdir())
+    print(Path.iterdir(Path'.'))
 
 
 def main():
