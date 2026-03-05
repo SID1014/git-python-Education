2de075dc031dc6a7a8513eb615334b620a569b55
SID1014 <siddhantdakhore143@gmail.com>
2026-03-05 11:39:23 +0530

    sixth stage

--- app/main.py
+++ app/main.py
@@ -67,16 +67,19 @@ def main():
         parent_sha = sys.argv[4]
         commit_sha = sys.argv[2]
         content = f"tree {commit_sha}\nparent {parent_sha}\nauthor {Author} <{email}> {timestamp} {timezone}\ncommiter {Author} <{email}> {timestamp} {timezone}\n\n{mesage}"
-        content = f"commit {len(content)} 0\{content}".encode('utf-8')
+        content = f"commit {len(content)} \0{content}".encode('utf-8')
         # print(content)
         hash = hashlib.sha1()
         hash.update(content)
         #hexaganol number of hash
         p = hash.hexdigest()
-        d = "./.git/objects"
-        with open(f'.git/objects/{p}','xb') as m:
-                m.write(zlib.compress(content))
-                m.close()
+        d = f'.git/objects/{p[:2]}'
+        if not Path(d).exists():
+            os.mkdir(d)
+        #writes zlib compressed binary data
+        with open(f'.git/objects/{p[:2]}/{p[2:]}','wb') as m:
+            m.write(zlib.compress(content))
+            m.close()
         print(p)
         
     else:
